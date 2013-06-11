748187f4efe2d09ee89e4a3115e0009a1016225a
CraigularB <Craigular.B@gmail.com>
2013-06-10 23:27:21 -0500

    Basic loop structure added.

--- simple-shell/shell.cpp
+++ simple-shell/shell.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+
+const std::string PROMPT = "$> ";
 
 int main()
 {
+	std::string input;
+	std::cout << PROMPT;
+	std::getline(std::cin, input);
+	std::cout << input << std::endl;
+	//making this quitable by command for now
+	while(input != "QUIT")
+	{
+		std::cout << PROMPT;
+		std::getline(std::cin, input);
+		std::cout << input << std::endl;
+	}
+	std::cout << "Thanks for using simple-shell" << std::endl;
 	return 0;
 }
